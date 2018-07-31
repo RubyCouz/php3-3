d6d6a277a69a631bda01e4fb03632ccad240631a
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-31 11:16:46 +0200

    correction

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-$number = 100;
-$number1 = 12;
+//définition des variable de départ
+$firstNumber = 100;
+$secondNumber = rand(1, 100);
  ?>
  <!DOCTYPE html>
  <html lang="fr" dir="ltr">
@@ -11,11 +12,15 @@ $number1 = 12;
    <body>
      <p>
      <?php
-while ($number > 10)
-{
-echo $number * $number1 . ' ' ;
- $number--;
-}
+     //définition de la boucle
+      while ($firstNumber >= 10){// tant que firstNumber est inférieur à 10
+      echo $firstNumber * $secondNumber . ' ' ; // affichage des résultats
+       $firstNumber--; // décrémentation  de la variable $firstNumber
+      }
+//variante avec un boucle for
+      for($firstNumber = 100; $firstNumber >= 10; $firstNumber--){ //pour fistnumber égale à 100, firstNumber inférieur à 100, décrémentation de $firstnumber
+        echo $firstNumber * $secondNumber . ' '; //affichage des résultats
+      }
       ?>
     </p>
    </body>
